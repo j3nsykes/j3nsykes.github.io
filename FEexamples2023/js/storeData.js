1f2bb1bcaed78752cca20e7b14ada4c78b808784
j3nsykes <31961310+j3nsykes@users.noreply.github.com>
2023-10-02 16:40:13 +0100

    complete API
    added API post functionality to feedback form

--- FEexamples2023/js/storeData.js
+++ FEexamples2023/js/storeData.js
@@ -1,14 +1,14 @@
-//read CSV data
-// async function apiLoad() {
-//     await fetch("http://functionalerrors.com/api").then(async res => await res.json())
-//         .then(async res => {
-//             console.log(res);
-
-//         });
-// }
+let inputName
+let shieldBool
+let inputChanges
+let inputMetric
+let inputAnalysis
+let inputPageID
+let hashCode
 
 // Example POST method implementation:
 async function postInput(data) {
+    console.log('send')
     // POST request using Fetch API
     await fetch('https://functionalerrors.com/api', {
         method: 'POST',
@@ -30,40 +30,39 @@ async function postInput(data) {
 }
 
 
-function sendData() {
-    document.getElementById('myForm').addEventListener('submit', function (event) {
-        // Prevent the form from submitting (prevents page refresh)
-        event.preventDefault();
 
-        const name = 'jen'
-        const understood = 5
-        const changes = 2
-        const analysis = 'good stuff'
-        const shield = true
-        const page_id = 3
+function sendData(input_name, understood, changes, shield, page_id, analysis) {
 
-        const testVals = {
-            "name": name,
-            "understood": understood,
-            "changes": changes,
-            "shield": shield,
-            "page_id": page_id,
-            "analysis": analysis
-        };
-        let sendValues = JSON.stringify(testVals)
-        console.log(sendValues)
-        postInput(testVals);
-    });
+    //debug
+    /*
+    input_name = 'jen'
+    understood = 5
+    changes = 2
+    analysis = 'good stuff'
+    shield = true
+    page_id = 3
+    */
+
+    const testVals = {
+        "name": input_name,
+        "understood": understood,
+        "changes": changes,
+        "shield": shield,
+        "page_id": page_id,
+        "analysis": analysis
+    };
+
+    //let sendValues = JSON.stringify(testVals)
+    //console.log(testVals)
+    postInput(testVals);
+
+
 }
 
-async function hashName() {
-    // Get the name from the input
-    //const name = document.getElementById("name").value;
-    const name = 'lula sykes'
-
+async function hashName(name) {
     // Convert the name to a Uint8Array
     const encoder = new TextEncoder();
-    const data = encoder.encode(name);
+    const data = encoder.encode(name); // Get the name from the input
 
     // Hash the name using SHA-256
     const hashed = await crypto.subtle.digest('SHA-256', data);
@@ -74,5 +73,135 @@ async function hashName() {
 
     // Display the hash
     //document.getElementById("hashedName").innerText = hashHex;
-    console.log("hashCode:" + hashHex)
+    //console.log("hashHex:" + hashHex)
+    hashCode = hashHex
+    // console.log("hashCode:" + hashCode)
+
 }
+
+function getCSV() {
+    document.getElementById('getCSV').addEventListener('click', function () {
+        console.log('get CSV')
+        getData();
+
+
+    });
+}
+
+async function getData() {
+    await fetch("https://functionalerrors.com/csv").then(async res => await res.text())
+        .then(async res => {
+            console.log(res);
+        });
+
+
+}
+
+
+
+
+function inputData() {
+    document.getElementById('myForm').addEventListener('submit', async function (event) {
+        // Prevent the form from submitting and the page from reloading
+        event.preventDefault();
+
+        // Get the selected radio button value
+        let radios = document.getElementsByName('choice');
+        let selectedValue = null;
+        for (let radio of radios) {
+            if (radio.checked) {
+                selectedValue = radio.value;
+                break;
+            }
+        }
+
+        // Get the text from the textarea
+        let textInput = document.getElementById('textAreaInput').value.trim(); // .trim() removes whitespace from start and end
+        const dropdown = document.getElementById('nameDropdown').value;
+        const shield = document.getElementById('shieldDropdown');
+        const pageID = document.body.id;
+        const numChanges = document.getElementById('numBox')
+        await hashName(dropdown)
+
+        // Only log values if conditions are met
+        if (selectedValue && textInput) {
+            // console.log("Selected radio button:", selectedValue);
+            // console.log("Text input:", textInput);
+            // console.log('Selected name:', dropdown);
+            // console.log('coded name:', hashCode);
+            // console.log('Page ID:', pageID);  // Outputs: "page1"
+            // console.log('num chnages:', numChanges.value)
+            //inputs
+            inputMetric = parseInt(selectedValue) //linker sclae
+            inputName = String(hashCode)
+            inputPageID = parseInt(pageID)
+            inputAnalysis = textInput
+            inputChanges = parseInt(numChanges.value)
+
+
+            if (shield.value != null) {
+                console.log('Feedback Shield Used:', shield.value)
+                if (shield.value == 'No') {
+                    shieldBool = false
+                }
+                else {
+                    shieldBool = true
+                }
+            }
+            console.log('shield val', shieldBool)
+
+            //send to API
+            sendData(inputName, inputMetric, inputChanges, shieldBool, inputPageID, inputAnalysis)
+
+        } else {
+            if (!selectedValue) {
+                console.log("Please select a score.");
+            }
+            if (!textInput) {
+                console.log("Please enter text into the textarea.");
+            }
+        }
+
+    });
+}
+
+function addNames() {
+    document.getElementById('addNameButton').addEventListener('click', function () {
+        const dropdown = document.getElementById('nameDropdown');
+        const newName = document.getElementById('newNameInput').value.trim();
+
+        if (newName) { // Only add if there's a non-empty name
+            const newOption = document.createElement('option');
+            newOption.value = newName;
+            newOption.textContent = newName;
+            dropdown.appendChild(newOption);
+
+            // Optionally, you can clear the input box after adding the name:
+            document.getElementById('newNameInput').value = '';
+        }
+    });
+}
+
+// Sample JSON data
+const namesJson = '["Jen","Lula","Doe","Smith"]';
+// Parse the JSON data
+const namesArray = JSON.parse(namesJson);
+// Get the dropdown element by ID
+const dropdown = document.getElementById('nameDropdown');
+
+// Function to populate the dropdown with the names
+function populateDropdown(data) {
+    // Clear existing options
+    dropdown.innerHTML = "";
+
+    data.forEach(name => {
+        const optionElement = document.createElement('option');
+        optionElement.value = name;
+        optionElement.innerText = name;
+        dropdown.appendChild(optionElement);
+    });
+}
+
+// Call the populateDropdown function with the namesArray
+populateDropdown(namesArray);
+
